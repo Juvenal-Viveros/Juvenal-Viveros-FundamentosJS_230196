59123b4042ae387c764cc7f1ae578c9e3b5afbfb
Juvenal <230196@utxicotepec.edu.mx>
2024-09-24 14:18:37 -0600

    Practica05
    Modificamos el Ejercicio3.js de la carpetaJavaScript

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,4 +1,5 @@
-const bg = "linear-gradient(11deg, rgba(2, 0, 36, 1) 0% rgba(9, 9, 121, 1) 33%, rgba(0, 212, 255, 1) 86%)";
+
+const bg = "linear-gradient(11deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 33%, rgba(0, 212, 255, 1) 86%)";
 const style_console = `background: ${bg}; color:white; border-radius: 6px; padding: 4px; font-size; 1.0rem; font-weight:bold`
 
 //Personalización de las salidas de consola
@@ -109,7 +110,7 @@ console.log(`Los datos del PRODUCTO son: \n"
         Comprador_Clave: 3216,
         Cantidad: 2,
         Estatus: "Carrito de Compra",
-
+        TipoPago:"Tarjeta De Credito"
     }
 
     //En base de a llos 3 objetos necesitamos calcular el costo de la compra y si le alcanza con sus saldo a favor
@@ -123,6 +124,40 @@ console.log(`Los datos del PRODUCTO son: \n"
     if (Costo_Compra < Cliente_SaldoActual) {
         console.log("El cliente tiene saldo suficiente");
     }
+    //Actualizar el valor de los Objetos
+    console.log("%c4.-Actualizar el valor de las propiedades de un objeto",style_console);
+    console.log(`El objeto actualmente tiene los siguentes valores: `)
+    console.log(JSON.stringify(Producto2,null,2));
+    //convierte el objeto a una cadena para evitar problemas con la referencia
+    console.log(`Por cuestiones de inflacion del costo del producto ha cambiado y debe ser actualizado... de $6,829.00 a $6.915.00`)
+    //Para modificar el valor de un objeto basta con igualar el valor de la propiedad deseada
+    Producto2.Precio=6915.00;
+    console.log(`Los nuevos valores del Producto son: `)
+    console.log(Producto2);
+    //¿Puedo cambiar no solo el valor, si no el tipo de dato de un objeto en javaScript?
+    console.log(`---------------------------------------------------------------`)
+    console.log(`El objeto actualmente tiene los siguentes valores`)
+    let tipoDisponibilidad=typeof(Producto.Disponibilidad)
+    console.log(`El tipo de dato de la Disponibilidad es: ${tipoDisponibilidad}`)
+    console.log(JSON.stringify(Producto2,null,2))//Disponibilidad Booleano
+    Producto2.Disponibilidad="sí";
+    let nuevoTipoDisponibilidad=typeof(Producto2.Disponibilidad)
+    console.log(Producto2);
+    console.log(`El nuevo tipo de Disponibilidad es: ${nuevoTipoDisponibilidad}`)
+    //Agregar nuevas propiedades a los objetos
+    console.log("%c5.-Agregar nuevas propiedades a los objetos",style_console);
+    //Para agregar una nueva propiedad utilizaremos el nombre del objeto los corchetes [] y el nuevo de la propiedad con su valor por defecto.
+    Comprador['Dirección']="Av. Benito Juárez No. 1525, Interior 4D, xicotepec de Juárez, Puebla, Mexico"
+    Comprador['Tipo']="nuevo Cliente"
+    Comprador['ActividadReciente']=true
+    Comprador['TotalCompras']=3516.25
+    console.log("Después de haber agregado las propiedades; Dirección, Tipo, ActividadReciente y TotalCompras...")
+    console.table(Comprador)
+    //Eliminar Propiedades existentes de un objeto
+    console.log("%c6.- Eliminar  propiedades existentes de un objeto", style_console);
+    console.log("La estructura y valores del objeto PEDIDO son previos a la modificación: ")
+    console.table(Pedido)
+    delete Pedido.TipoPago
+    console.log("Despues de la modificación...")
+    console.table(Pedido)
     
-    //PRÁCTICA 4 fusionada con los 8 tipos de datos.
-    //Hacer commit si te salió todo esto.
